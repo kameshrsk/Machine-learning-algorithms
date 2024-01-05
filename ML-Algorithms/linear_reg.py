4437fab5b25df8989c2a18d26be2aac4c2c3cc9d
kameshrsk <56502839+kameshrsk@users.noreply.github.com>
2024-01-05 12:06:33 +0530

    Update linear_reg.py

--- ML-Algorithms/linear_reg.py
+++ ML-Algorithms/linear_reg.py
@@ -1,1 +1,8 @@
-print("Hello Wold!")
+import os
+
+def main():
+  bn=os.getenv('BUILD_NUMBER', 'Unknown')
+  print(f'This is a python build. The Build NUmber is:{bn}')
+
+if __name__=='__main__':
+  main()
